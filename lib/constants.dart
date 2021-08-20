fa04534340bcc4e14cf25aa5bf695412bac9612b
VICHA <vanvick24@yahoo.com>
2021-08-21 00:28:40 +0100

    user inputs

---
+++ lib/constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+final textInputDecoration = InputDecoration(
+  hintStyle: TextStyle(fontSize: 20),
+  border: InputBorder.none,
+  filled: true,
+  fillColor: Colors.grey[250],
+  isDense: true,
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.transparent, width: 0.0),
+    borderRadius: BorderRadius.circular(15.0),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.transparent, width: 0.0),
+    borderRadius: BorderRadius.circular(15.0),
+  ),
+);
+const Color primary = Colors.black;
+const  red = Colors.red;
+const Color white = Colors.white;
+const Color black = Colors.black;
+const Color grey = Colors.grey;
+const Color green = Colors.green;
+const Color purple = Colors.purple;
+Color active = Colors.orange;
+Color disabled = Colors.grey;
